98e55c56b44b76eebbeb8b2f72ccd9ac16771031
nikola.cvetkovic <cvelekraljevo@gmail.com>
2019-09-11 16:27:03 +0200

    first commit

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of clicks {{ stringCounter }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["doubleCounter", "stringCounter"]),
+    ourOwn() {}
+  }
+  //   computed: {
+  //     counter() {
+  //       return this.$store.getters.doubleCounter;
+  //     },
+  //     clicks() {
+  //       return this.$store.getters.stringCounter;
+  //     }
+  //   }
+};
+</script>
